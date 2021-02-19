ad55076e3c369463f0e278529d0131abc873452c
Peter Gemmell <petergemmell65@hotmail.com>
2021-02-19 17:46:24 +0000

    contract tests passing

--- truffle-config.js
+++ truffle-config.js
@@ -1,25 +1,30 @@
-// module.exports = {
-//   // See <http://truffleframework.com/docs/advanced/configuration>
-//   // for more about customizing your Truffle configuration!
-//   networks: {
-//     development: {
-//       host: "127.0.0.1",
-//       port: 7545,
-//       network_id: "*" // Match any network id
-//     },
-//     develop: {
-//       port: 8545
-//     }
-//   }
-// };
-
 module.exports = {
-  // your existing config goes here
-  // don't forget to put comma on the last element before proceeding to next line
-
   compilers: {
     solc: {
       version: "0.4.22"
     }
+  },
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    develop: {
+      port: 8545
+    }
   }
-}
+};
+
+// module.exports = {
+//   // your existing config goes here
+//   // don't forget to put comma on the last element before proceeding to next line
+//
+//   compilers: {
+//     solc: {
+//       version: "0.4.22"
+//     }
+//   }
+// }
